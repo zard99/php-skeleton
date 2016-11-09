96a324ee819b823728e198d6fe1f1166aab36d6c
zard99 <keccoz99@gmail.com>
2016-11-09 07:39:46 +0100

    Aggiunta la pagina principale del sito. Rimangono da implementare le media-queries, per cui il sito per ora è visitabile solo da desktop

--- partials/head.php
+++ partials/head.php
@@ -5,8 +5,28 @@
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	  <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	  <title><?php echo($title); ?></title>
-	  <link rel="stylesheet" href="/assets/css/main.css">
+	  <link rel="stylesheet" href="/assets/css/main.css" type="text/css">
+	  <link href="https://fonts.googleapis.com/css?family=Actor" rel="stylesheet">
 	  <!-- La parte del javascript per ora non ci interessa <script type="text/javascript" src="/assets/js/app.js" async></script> -->
 	</head>
 	<body>
-		<h1> Hello world! </h1>
+		<div id="content">
+			<div id="separator-left">
+				<div id="text-head"> 
+					Recycle in <br> Barletta
+				</div>
+				<div id="searchcontainer">
+					<input type="text" name="searchbar" id="inputbar" placeholder="Es. : Butstina di patatine">
+					<br>
+					<button id="searchbutton"> Cerca </button>
+				</div>
+				<p id="text-bottom">
+					Riciclare é importante, ma non sempre facile. <br>
+					Recycle in Barletta ti aiuta: non sai dove mettere  qualcosa? <br>
+					Inserisci l’oggetto in questa pagina, noi ti diremo dove! <br>
+				</p>
+			</div>
+			<div id="separator-right">
+				<img src="assets/images/recycle.png" id="recycle-img">
+			</div>
+		</div>
