74285128ac4009213d6e7d0fde7fbf8f5b74d97c
zard99 <keccoz99@gmail.com>
2016-11-10 21:10:24 +0100

    Aggiunta la stampa dell'oggetto cercato nella pagina web. Adesso l'oggetto cercato viene visualizzato a schermo.

--- result.php
+++ result.php
@@ -9,22 +9,22 @@
   <input type="submit" value="Submit">
 </form>-->
 
-<div id="content">
+		<div id="content">
 			<img src="assets/images/recycle.png" id="recycle-img-mobile">
-			<div id="section-right" class="line-right">
-				<img src="assets/images/bin-plastic.png" id="bin1">
+			<div id="section-line-container" class="line-slash-right line-properties">
+				<img src="assets/images/bin-plastic.png" id="bin-container">
 				<img src="assets/images/recycle.png" id="recycle-img" class="img-left">
 			</div>
-			<div id="section-left">
-				<div id="text-head" class="pos-right"> 
+			<div id="section-content-container">
+				<div id="text-head" class="text-head-to-right"> 
 					Recycle in <br> Barletta
 				</div>
 				<div id="result-container">
-					Ricicla <strong> Tetrapack </strong> <br>
+					Ricicla <strong> <?php echo($_GET['object']); ?> </strong> <br>
 					come : <strong> Plastica </strong>
 				</div>
 				<form action="index.php" style="z-index: 2;">
-				<button id="backbutton" class="button"> Indietro </button>
+				<button id="back-button" class="button button-dark button-to-right button-resize-paint-green"> Indietro </button>
 				</form>
 			</div>
 		</div>
