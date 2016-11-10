cc2c1a902fd8195963075d639b5e4bcade9b80e2
zard99 <keccoz99@gmail.com>
2016-11-10 21:03:06 +0100

    Invertito l'ordine dei bottoni. Adesso l'ordine è | Invia | e | Indietro |, per far si che quando l'utente preme Invio, viene automaticamente attivato il bottone | Invia |. Aggiunto il controllo per verificare la validità della email (non se effettivamente esiste), usando una Regular Expression.

--- email.php
+++ email.php
@@ -4,14 +4,16 @@
   Ti invieremo al più presto un'email a: <b><?php echo($_POST['email']); ?></b>
 </p>-->
 
-<div id="content">
+		<div id="content">
 			<img src="assets/images/recycle.png" id="recycle-img-mobile">
-			<div id="section-right" class="line-right">
+			<div id="section-line-container" class="line-slash-right line-properties">
+
 				<div id="sad-face"> :( </div>
 				<img src="assets/images/recycle.png" id="recycle-img" class="img-left">
 			</div>
-			<div id="section-left">
-				<div id="text-head" class="pos-right"> 
+			<div id="section-content-container">
+
+				<div id="text-head" class="text-head-to-right"> 
 					Recycle in <br> Barletta
 				</div>
 				<div id="result-container">
@@ -20,15 +22,15 @@
 				<p class="text">
 					Ci stiamo informando! Inserisci la tua email, così ti possiamo dire come
 				</p>
-				<div id="contbtn">
-					<form action="email_success.php" method="get">
-						<input type="text" name="emailbar" id="emailbar" placeholder="Inserisci qui la tua email">
+				<div id="button-container">
+					<form action="email_success.php" method="POST">
+						<input type="text" name="email" id="email-bar" class="bar-property" placeholder="Inserisci qui la tua email" pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" required>
 						<br>
-						<button id="backbtn" formaction="index.php"> Indietro </button>
-						<input type="submit" id="subm" value="Invia">
+						
+						<input type="submit" id="submit-button" class="button button-dark float-side-left margin-side-left button-fixed-width" value="Invia">
+						<button id="back-button" class="button button-light float-side-right margin-side-right button-fixed-width" formnovalidate="formnovalidate" formaction="index.php"> Indietro </button>
 					</form>
 				</div>
-				
 			</div>
 		</div>
 
